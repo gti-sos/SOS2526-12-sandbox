f34d493d301983638240a071c3963a0ddb7d546c
Lucca-Pereira-2 <lucperhea@alum.us.es>
2026-02-17 21:05:40 +0100

    Clase L04

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+// const express = require("express");
+// const app = express();
+// const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello")
-});
+// app.get("/", (req, res) => {
+//     res.send("Hello")
+// });
 
-app.listen(port, () => {
-    console.log("Server running on port:" + port)
-});
+// app.listen(port, () => {
+//     console.log("Server running on port:" + port)
+// });
+
+let cool = require("cool-ascii-faces");
+//console.log(cool())
+
+//EJEMPLO DE SERVIDOR
+let express = require("express");
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send(`<html><body><h1>
+            ${cool()}
+            <h1><body><html>`)
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
