09c596ce39632a052404b02978baaafed68a8244
Lucca-Pereira-2 <lucperhea@alum.us.es>
2026-02-23 16:03:50 +0100

    update

--- Clase 20-2/index.js
+++ Clase 20-2/index.js
@@ -1,28 +1,29 @@
-//EJEMPLO DE SERVIDOR
-let express = require("express");
-
-const app = express()
-
+let express = require('express');
+let bodyParser = require('body-parser')
 let BASE_URL_API = "/api/v1";
+let PORT = process.env.PORT || 3000;
 
-app.use("/", express.static("./static"));
+const app = express();
 
-let contacts = [
-    {
-        "name":"peter",
-        "phone" :12341234
-    },
-    {
-        "name":"Jhon",
-        "phone" :12341234
-    }
-]
+app.use("/",express.static("./static"));
+app.use(bodyParser.json());
 
-app.get(BASE_URL_API+"/contacts", (req, res) => {
-    res.send(JSON.stringify(contacts, null, 2));
+let contacts = [{ name: "peter", phone: 123456},{ name: "john",   phone: 23456}];
 
-})
+app.get(BASE_URL_API+"/contacts", (req,res)=>{
+  let jsonData = JSON.stringify(contacts,null,2);
+  console.log(`JSON Data to be sent: ${jsonData}`);
+  res.send(jsonData);
+});
+app.post(BASE_URL_API+"/contacts", (req,res)=>{
+  let newContact = req.body;
+  console.log(`New contact received: ${JSON.stringify(newContact,null,2)}`);
+  contacts.push(newContact);
+  res.sendStatus(201,"CREATED");
+});
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
